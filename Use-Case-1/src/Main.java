3078ef7648533ca63d19379cd35181f1afcdcbe3
Oscar Ricaud <oiricaud@miners.utep.edu>
2016-11-16 13:37:56 -0700

    I have included the Javadoc for each class, document the author, the version.

--- Use-Case-1/src/Main.java
+++ Use-Case-1/src/Main.java
@@ -1,11 +1,14 @@
 import java.util.Scanner;
-
+/**
+ * Created by Oscar I. Ricaud on 11/16/16.
+ * Main.java V1
+ */
 public class Main {
 
     public static void main(String[] args) {
         Scanner input =new Scanner(System.in);
         Student st = new Student();
-        System.out.println("Hello there are you register with our System?");
+        System.out.println("Hey there, hi there, are you register with our System?");
         st.register(input.next());
     }
 }
--- Use-Case-1/src/Student.java
+++ Use-Case-1/src/Student.java
@@ -1,13 +1,18 @@
+import java.util.LinkedList;
 import java.util.Scanner;
-
 /**
- * Created by oscarricaud on 11/16/16.
+ * Created by Oscar I. Ricaud on 11/16/16.
+ * Student.java V1
  */
 public class Student {
     Scanner input =new Scanner(System.in);
     private String id;
     private String first_name;
     private String last_name;
+    private LinkedList<String> classes;
+
+    public Student() {
+    }
 
     public String getFirst_name() {
         return first_name;
@@ -33,6 +38,9 @@ public class Student {
         this.last_name = last_name;
     }
 
+    /**
+     * @param userInput
+     */
     public void register(String userInput) {
 
         // Register for new students
@@ -48,21 +56,55 @@ public class Student {
         }
         else{
             System.out.println("\n Great! Since you are already register with our system, then you can skip" +
-                    " the registration for new students. \n What would you like to do today? Register, Check Transcript or" +
+                    " the registration for new students. \n What would you like to do today? Select a class, Check Transcript or " +
                     "Drop a class?");
-            input.next();
+            String whatToDo = input.nextLine();
+            if(whatToDo.equalsIgnoreCase("Select a class")){
+                selectClass();
+            }
+            if(whatToDo.equalsIgnoreCase("Check Transcript")){
+                checkTranscript();
+            }
+            if(whatToDo.equalsIgnoreCase("Drop a class")){
+                drop_a_class();
+            }
         }
     }
+
     public void verifyInfo() {
         System.out.println("\n So you say your first name is: " + getFirst_name() + " your last name is: " + getLast_name()
                 + " and your ID number is " + getId() + " is this correct?");
             register(input.next());
     }
 
-    public void checkTranscript(){
+    private void selectClass() {
+        System.out.println("\n Please tell me how many class you want to register for");
+        LinkedList<String> holdOnClasses = new LinkedList<String>();
+        int numOfClasses = input.nextInt();
+        for(int i = 1; i < numOfClasses + 1; i++){
+            System.out.println("For class "  + i + " please tell me the name of the course");
+            holdOnClasses.add(input.next());
+        }
+        setClasses(holdOnClasses);
+    }
 
+    public void checkTranscript(){
+        if(getClasses().isEmpty()){
+            System.out.println("It appears that your transcript is empty, please try to register for classes " +
+                    " before you attempt to view your transcript");
+        }
+        else{
+            getClasses();
+        }
     }
     public void drop_a_class(){
+        System.out.println("\n What class would you like to drop?");
+    }
 
+    public LinkedList getClasses() {
+        return classes;
+    }
+    public void setClasses(LinkedList<String> classes) {
+        this.classes = classes;
     }
 }
